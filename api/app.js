b8dd3b21c95fd711d8785bbd039c881b96da66e2
Owlaukka <olli.laukkanen@protonmail.com>
2020-08-08 18:48:53 +0300

    cleaned up backend from unused stuff and made som stuff only used in testing environment

--- api/app.js
+++ api/app.js
@@ -1,9 +1,6 @@
-'use strict';
 require('dotenv').config();
 const path = require('path');
 const cors = require('fastify-cors');
-const AutoLoad = require('fastify-autoload');
-const fastifyStatic = require('fastify-static');
 const GQL = require('fastify-gql');
 
 const schema = require('./graphql');
@@ -11,25 +8,21 @@ const schema = require('./graphql');
 module.exports = (fastify, opts, next) => {
   fastify.register(cors, { origin: true });
 
-  // Serve Frontend files
-  fastify.register(fastifyStatic, {
-    root: path.resolve(__dirname, '../frontend/dist'),
-  });
-
   fastify.register(GQL, {
     schema,
     graphiql: process.env.NODE_ENV !== 'production',
   });
 
-  // Autoload various cross-cutting plugins like auth etc.
-  fastify.register(AutoLoad, {
-    dir: path.join(__dirname, 'plugins'),
-    options: { ...opts },
-  });
-
-  // Used in tests and CI to detect by wait-on library that the server is up and pipeline can continue
+  // Testing only routes
   if (process.env.NODE_ENV === 'test') {
-    fastify.head('/', async function (request, reply) {
+    // Serve Frontend files in testing
+    // eslint-disable-next-line global-require, import/no-extraneous-dependencies
+    fastify.register(require('fastify-static'), {
+      root: path.resolve(__dirname, '../frontend/dist'),
+    });
+
+    // Used in tests and CI to detect by wait-on library that the server is up and pipeline can continue
+    fastify.head('/', async (request, reply) => {
       reply.send({ test: 1 });
     });
   }

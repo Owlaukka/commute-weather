6662d1ddd637c7d124ca66f8370475dd20620a64
Owlaukka <olli.laukkanen@protonmail.com>
2020-08-13 23:32:41 +0300

    fixed tests

--- frontend/cypress/integration/e2e/weatherCardList-e2e-spec.js
+++ frontend/cypress/integration/e2e/weatherCardList-e2e-spec.js
@@ -1,5 +1,11 @@
 describe('Weather card list', () => {
-  it('should have correct temperature on temperature cards', () => {
+  beforeEach(() => {
+    // TODO: maybe make this global in tests somehow
+    // mock time to be able to use mocked external responses that would otherwise change over time
+    cy.clock(new Date('2020-07-27T18:00:00').getTime());
+  });
+
+  it('should have correct temperature on temperature cards and correct amount of cards by default', () => {
     cy.visitWithMockGeolocation('/', {
       latitude: 60.2,
       longitude: 25.0,
@@ -9,15 +15,23 @@ describe('Weather card list', () => {
 
     cy.get('[data-testid=weather-info-card-list]')
       .children()
-      .should('have.length', 2);
+      .should('have.length', 4);
 
     cy.get('[data-testid=weather-card-temp]')
       .first()
-      .should('have.text', '18.4°C');
+      .should('have.text', '17.6°C');
 
     cy.get('[data-testid=weather-card-temp]')
       .eq(1)
-      .should('have.text', '17.6°C');
+      .should('have.text', '17.1°C');
+
+    cy.get('[data-testid=weather-card-temp]')
+      .eq(2)
+      .should('have.text', '16.3°C');
+
+    cy.get('[data-testid=weather-card-temp]')
+      .eq(3)
+      .should('have.text', '16.4°C');
   });
 
   it('should let user set a new planned commute value and update temperatures accordingly', () => {
@@ -33,10 +47,18 @@ describe('Weather card list', () => {
 
     cy.get('[data-testid=weather-card-temp]')
       .first()
-      .should('have.text', '17°C');
+      .should('have.text', '17.7°C');
 
     cy.get('[data-testid=weather-card-temp]')
       .eq(1)
-      .should('have.text', '17.7°C');
+      .should('have.text', '17.1°C');
+
+    cy.get('[data-testid=weather-card-temp]')
+      .eq(2)
+      .should('have.text', '16.3°C');
+
+    cy.get('[data-testid=weather-card-temp]')
+      .eq(3)
+      .should('have.text', '16.4°C');
   });
 });

bbf26c61168ab954b8164daab9fd84c04a8964c8
Owlaukka <olli.laukkanen@protonmail.com>
2020-09-13 21:54:20 +0300

    switched to yarn 1 and hopefully fixed custom browser detection by adding error handling

--- frontend/cypress/plugins/index.js
+++ frontend/cypress/plugins/index.js
@@ -3,25 +3,29 @@ const webpackPreprocessor = require('@cypress/webpack-preprocessor');
 const webpackConfig = require('../../webpack/webpack.test');
 
 const findBrowser = () => {
-  const browserPath = '/usr/bin/brave-nightly';
+  const browserPath = process.env.BRAVE_PATH || '/usr/bin/brave-nightly';
 
-  return execa(browserPath, ['--version']).then((result) => {
-    // STDOUT will be like "Brave Browser 77.0.69.135"
-    const [, version] = /Brave Browser (\d+\.\d+\.\d+\.\d+)/.exec(
-      result.stdout
-    );
-    const majorVersion = parseInt(version.split('.')[0], 10);
+  return execa(browserPath, ['--version'])
+    .then((result) => {
+      // STDOUT will be like "Brave Browser 77.0.69.135"
+      const [, version] = /Brave Browser (\d+\.\d+\.\d+\.\d+)/.exec(
+        result.stdout
+      );
+      const majorVersion = parseInt(version.split('.')[0], 10);
 
-    return {
-      name: 'Brave',
-      channel: 'nightly',
-      family: 'chromium',
-      displayName: 'Brave',
-      version,
-      path: browserPath,
-      majorVersion,
-    };
-  });
+      return {
+        name: 'Brave',
+        channel: 'nightly',
+        family: 'chromium',
+        displayName: 'Brave',
+        version,
+        path: browserPath,
+        majorVersion,
+      };
+    })
+    .catch(() => {
+      return {};
+    });
 };
 /**
  * @type {Cypress.PluginConfig}
